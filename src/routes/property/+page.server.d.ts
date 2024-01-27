511e89a97975eaea3527dc6837d577b4f1c56bb2
farhienza haikal <farhienza.haikal@grorental.com>
2024-01-27 16:30:23 +0700

    fixing environment variable
    it need to use PUBLIC_ prefix and uses from '$env/static/public'

---
+++ src/routes/property/+page.server.d.ts
@@ -0,0 +1,31 @@
+export declare const load: () => Promise<{
+    data: {
+        email: string | null;
+        id: string;
+        timezone: "Asia/Jakarta" | null;
+        property_name: string | null;
+        slug: string | null;
+        address: string | null;
+        currency: string | null;
+        city: string | null;
+        state: string | null;
+        country: string | null;
+        zip_code: string | null;
+        phone: string | null;
+        is_active: boolean | null;
+        min_stay_type: string | null;
+        acc_channels_count: string | null;
+        latitude: string | null;
+        longitude: string | null;
+        location_precision: string | null;
+        property_type: string | null;
+        property_category: string | null;
+        website: string | null;
+        logo_url: string | null;
+        default_cancellation_policy_id: string | null;
+        hotel_policy_id: string | null;
+        default_tax_set_id: string | null;
+        profile_id: string | null;
+        created_at: Date | null;
+    }[];
+}>;
