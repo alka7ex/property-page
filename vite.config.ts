4b47e72dba4dd4b7fdf9dc064bfc01fec190a342
farhienza haikal <farhienza.haikal@grorental.com>
2024-01-28 13:43:18 +0700

    adding sveltekit adapter for ilang

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { paraglide } from "@inlang/paraglide-js-adapter-sveltekit/vite"
 import { defineConfig } from 'vitest/config';
 import path from "path";
 
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		paraglide({
+			project: "./project.inlang",
+			outdir: "./src/paraglide",
+		}),],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	},
