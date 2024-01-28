f575585327028f98dcf0a049683d460502d07395
farhienza haikal <farhienza.haikal@grorental.com>
2024-01-28 13:56:24 +0700

    adding hooks config for inlang

--- src/routes/(main)/+layout.svelte
+++ src/routes/(main)/+layout.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
 	import Navbar from '$lib/components/page/main-page/Navbar.svelte';
 	import '../../app.pcss';
+	import { ParaglideJS } from '@inlang/paraglide-js-adapter-sveltekit';
+	import { i18n } from '$lib/i18n.js';
 </script>
 
 <div class="bg-background h-screen">
 	<Navbar/>
-
+<ParaglideJS {i18n}>
 	<slot />
+</ParaglideJS>
 </div>
