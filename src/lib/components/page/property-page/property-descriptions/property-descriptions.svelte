20f87195b7aaa829e325fa67d68e5c2ac44848ea
farhienza haikal <farhienza.haikal@grorental.com>
2024-02-03 17:04:22 +0700

    fixing facilities where it is too long

--- src/lib/components/page/property-page/property-descriptions/property-descriptions.svelte
+++ src/lib/components/page/property-page/property-descriptions/property-descriptions.svelte
@@ -1,41 +1,46 @@
 <script lang="ts">
 	import * as Card from '$lib/components/ui/card/index.js';
-	import * as m from "$paraglide/messages"
+	import * as m from '$paraglide/messages';
 	export let propertyDetails: any;
 </script>
 
-<Card.Root class="border shadow-lg bg-[#FEF2F4] border-none">
+<Card.Root class="border border-none bg-[#FEF2F4] shadow-lg">
 	<Card.Header class="">
 		<h1 class=" text-2xl font-bold">{propertyDetails.property_name}</h1>
 	</Card.Header>
 	<Card.Content class="border-none">
 		<Card.Description class="border-none text-black">
 			<p class="my-2">
-				{propertyDetails.content.description}
+			  {propertyDetails.content.description}
 			</p>
+		  
 			<h3 class="my-2 text-xl font-bold">{m.propertydetails_descriptions_facilities()}</h3>
-			{#each propertyDetails.content.facilities as facilitiesList}
-			<ul class="my-1 list-inside list-disc">
-				<li>{facilitiesList.facilities_name}</li>
-			</ul>
-			{/each}
+		  
+			<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-0">
+			  {#each propertyDetails.content.facilities as facilitiesList}
+				<ul class="my-1 list-inside list-disc">
+				  <li>{facilitiesList.facilities_name}</li>
+				</ul>
+			  {/each}
+			</div>
+		  
 			<h3 class="my-2 text-xl font-bold">{m.propertydetails_descriptions_property_policy()}</h3>
 			<p class="my-2">
-				{propertyDetails.property_policy}
+			  {propertyDetails.property_policy}
 			</p>
+		  
 			<h3 class="my-2 text-xl font-bold">{m.propertydetails_descriptions_cancellation_policy()}</h3>
 			<p class="my-2">
-				{propertyDetails.cancellation_policy}
+			  {propertyDetails.cancellation_policy}
 			</p>
+		  
 			<h3 class="my-2 text-xl font-bold">{m.propertydetails_descriptions_address()}</h3>
 			<p class="my-2">
-				<a href={propertyDetails.google_maps_url}>
-					{propertyDetails.address}
-				</a>
-                
+			  <a href={propertyDetails.google_maps_url}>
+				{propertyDetails.address}
+			  </a>
 			</p>
-            
-		</Card.Description>
+		  </Card.Description>
 	</Card.Content>
 </Card.Root>
 
